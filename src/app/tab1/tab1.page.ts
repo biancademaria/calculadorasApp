5969008addc0cf43642e369d3a8ba6fa9e697a23
biancademaria <bianca.maria@etec.sp.gov.br>
2020-06-10 16:24:58 -0300

    Calculadora Simples completa
    Calculadora simples completa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  public primeiroValor: number;
+  public segundoValor: number;
+
+  public resultado: number;
+
   constructor() {}
+
+  public soma() {
+    this.resultado = this.primeiroValor + this.segundoValor;
+  }
+
+  public subtracao() {
+    this.resultado = this.primeiroValor - this.segundoValor;
+  }
+
+  public multiplicacao() {
+    this.resultado = this.primeiroValor * this.segundoValor;
+  }
+
+  public divisao() {
+    this.resultado = this.primeiroValor / this.segundoValor;
+  }
+
+  public limpar() {
+    this.primeiroValor = null;
+    this.segundoValor = null;
+    this.resultado = null;
+  }
 
 }
